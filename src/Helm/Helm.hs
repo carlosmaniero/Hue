b1c9f6be6b6d5f9b25b077dc4d3303e981dcf58d
Carlos Maniero <carlosmaniero@gmail.com>
2017-10-31 07:33:50 -0200

    Add the parallel processing of a list with async update
    A validation was added to doesn't process anything given an empty list
    of operations.

--- src/Helm/Helm.hs
+++ src/Helm/Helm.hs
@@ -1,5 +1,6 @@
 module Helm
     ( processIO
+    , processIOParallel
     , processIOBulkParallel
     ) where
 
@@ -15,12 +16,32 @@ runOperationAsync channel operation = forkIO $ do
 
 
 processIOBulkParallel :: (msg -> model -> model) -> model -> ([result] -> msg) -> [IO result] -> IO model
+processIOBulkParallel _ model _ [] = do
+    return model
 processIOBulkParallel update model resultMsg operations = do
     channel <- newEmptyMVar
     mapM_ (runOperationAsync channel) operations
 
     results <- replicateM (length operations) $ takeMVar channel
     return $ update (resultMsg results) model
+
+
+updateParallelModel :: Int -> MVar result -> (msg -> model -> model) -> model -> (result -> msg) -> IO model
+updateParallelModel 0 _ _ model _ = do
+    return model
+updateParallelModel times channel update model resultMsg = do
+    result <- takeMVar channel
+    let updatedModel = update (resultMsg result) model
+    updateParallelModel (times - 1) channel update updatedModel resultMsg
+
+
+processIOParallel :: (msg -> model -> model) -> model -> (result -> msg) -> [IO result] -> IO model
+processIOParallel _ model _ [] = do
+    return model
+processIOParallel update model resultMsg operations = do
+    channel <- newEmptyMVar
+    mapM_ (runOperationAsync channel) operations
+    updateParallelModel (length operations) channel update model resultMsg
 
 
 processIO :: (msg -> model -> model) -> model -> IO msg -> IO model
--- test/ProcessIOSpec.hs
+++ test/ProcessIOSpec.hs
@@ -6,29 +6,50 @@ import Test.Hspec
 
 processIOSpec :: SpecWith ()
 processIOSpec = do
+    let initModel = Model { expected = 0, updates = 0 }
     describe "Given an IO Operation and an update function" $ do
         it "should call the update with the IO result message" $ do
-            model <- processIO update (Model { expected = 99 }) theAnswer
+            model <- processIO update initModel theAnswer
             expected model `shouldBe` 42
+            updates model `shouldBe` 1
     describe "Given a list of IO Operation and an update function" $ do
-        it "should call the list of IO operation and returns these list" $ do
-            model <- processIOBulkParallel update (Model { expected = 99 }) ProcessedList [theNumber, theAnotherNumber]
-            expected model `shouldBe` 2000
+        describe "When I expect the parallel operation to update the full list of result" $ do
+            it "should process all the list of IO operation" $ do
+                model <- processIOBulkParallel update initModel ProcessedList [theNumber, theAnotherNumber]
+                expected model `shouldBe` 2000
+                updates model `shouldBe` 1
+            it "should do nothing given a empty list" $ do
+                model <- processIOBulkParallel update initModel ProcessedList []
+                expected model `shouldBe` 0
+                updates model `shouldBe` 0
+        describe "When I expect the parallel operation to update each operation" $ do
+            it "should process the list and return as soon as the operation is done" $ do
+                model <- processIOParallel update initModel Completed [theNumber, theAnotherNumber]
+                updates model `shouldBe` 2
+                expected model `shouldBe` 2000
+            it "should do nothing given a empty list" $ do
+                model <- processIOParallel update initModel Completed []
+                expected model `shouldBe` 0
+                updates model `shouldBe` 0
 
 
 data Msg
   = Completed Int
   | ProcessedList [Int]
 
-data Model = Model { expected :: Int }
+data Model = Model { expected :: Int
+                   , updates  :: Int }
 
 update :: Msg -> Model -> Model
 update msg model =
+    let
+        calls = 1 + updates model
+    in
     case msg of
       Completed value ->
-          Model { expected = value }
+          Model { expected = value + expected model, updates = calls }
       ProcessedList list ->
-          Model { expected = foldl (+) 0 list }
+          Model { expected = foldl (+) 0 list, updates = calls }
 
 
 theAnswer :: IO Msg
